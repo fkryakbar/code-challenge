e3c46f8d7e742b6d2dcfdf9f9ab6d3c02b8b3e61
fkrydev <fikri.afa289@gmail.com>
2024-02-03 22:24:15 +0800

    22

---
+++ 22-vowel-count.ts
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/54ff3102c1bad923760001f3/train/typescript
+
+
+export class Kata {
+    static getCount(str: string): number {
+        return str.split('').filter((a) => ['a', 'e', 'i', 'o', 'u'].includes(a)).length
+    }
+}
+
+console.log(Kata.getCount('abracadabra'));
