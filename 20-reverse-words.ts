1b66a26390ce5c325bc925326db8fd1bba492813
fkrydev <fikri.afa289@gmail.com>
2024-02-03 16:52:18 +0800

    update

--- 20-reverse-words.ts
+++ 20-reverse-words.ts
@@ -1,3 +1,4 @@
+// https://www.codewars.com/kata/5259b20d6021e9e14c0010d4/train/typescript
 export const reverseWords = (str: string): string => str.split(' ').map((words: string): string => words.split('').reverse().join('')).join(' ');
 
 
